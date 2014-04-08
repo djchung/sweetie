bcae00f7e178902e1dbbaddf7e0489d492900e8c
DJ Chung <djchung3@gmail.com>
2014-04-08 01:02:48 -0700

    show character limit

--- src/com/codepath/apps/sweetie/ComposeActivity.java
+++ src/com/codepath/apps/sweetie/ComposeActivity.java
@@ -5,9 +5,13 @@ import org.json.JSONObject;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnKeyListener;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
@@ -15,11 +19,38 @@ import com.loopj.android.http.JsonHttpResponseHandler;
 
 public class ComposeActivity extends Activity {
 
+	private Menu menu;
+	private EditText tweetCompose;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_compose);
-		EditText tweetCompose = (EditText) findViewById(R.id.etStatus);
+		tweetCompose = (EditText) findViewById(R.id.etStatus);
+		tweetCompose.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				int length = tweetCompose.getText().length();
+				int charLeft = 140 - length;
+				MenuItem charLimitItem = menu.findItem(R.id.charLimit);
+				charLimitItem.setTitle(String.valueOf(charLeft));
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
 		if (tweetCompose.requestFocus()) {
 			showKeyboardForView(tweetCompose);
 		}
@@ -34,6 +65,7 @@ public class ComposeActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.compose, menu);
+		this.menu = menu;
 		return true;
 	}
 	
